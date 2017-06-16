dc76241d7ab07d6959dc38f411b46f223b998bf6
EwelinaDziedzina <ewelina-213@wp.pl>
2017-06-16 12:29:33 +0100

    Code updated

--- Strings/CharacterAndWordCount.java
+++ Strings/CharacterAndWordCount.java
@@ -14,21 +14,23 @@ public class CharacterAndWordCount {
 		//char [] letters = sentence.toCharArray();
 		String [] words = sentence.split(" ");
 		System.out.println("Words: " + words.length);
+		System.out.println("Characters (with spaces): " + sentence.length());
 		
 		
 		int total = 0;
-		int i = 0;
-		for(; i<words.length; i++){
+		for(int i = 0; i<words.length; i++){
 			total = total + words[i].length();
 			}
 		
-		if(sentence.charAt(sentence.length() - 1) == '.'){
+		System.out.println("Characters (without spaces): " + (total));
+		
+		/*if(sentence.charAt(sentence.length() - 1) == '.'){
 		System.out.println("Characters (with spaces): " + (sentence.length()-1));
 		System.out.println("Characters (without spaces): " + (total-1));
 		}else{
 		System.out.println("Characters (with spaces): " + sentence.length());
 		System.out.println("Characters (without spaces): " + (total));
-		}
+		}*/
 	}
 }
 
